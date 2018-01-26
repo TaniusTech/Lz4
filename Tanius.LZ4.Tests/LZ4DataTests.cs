5765dd64542bda67caf0049551b8468356ed17e2
Karl Schulze <karl@taniustech.com>
2018-01-26 10:31:06 -0800

    Add two sample files that need to be read from.

---
+++ Tanius.LZ4.Tests/LZ4DataTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using LZ4;
+using NUnit.Framework;
+
+namespace Tanius.LZ4.Tests
+{
+    public class LZ4DataTests
+    {
+        [Test]
+        public void ReadSample1()
+        {
+            var fileName = @"C:\Users\karl\Visual Studio\External Tanius\LZ4\Tanius.LZ4.Tests\Data\test1.pcap.lz4"; // Change based on file structure of local machine
+
+            using (var fr = File.OpenRead(fileName))
+            using (var zr = new LZ4Stream(fr, LZ4StreamMode.Decompress))
+            using (var ms = new MemoryStream())
+            {
+                zr.CopyTo(ms);
+            }
+        }
+        [Test]
+        public void ReadSample2()
+        {
+            var fileName = @"C:\Users\karl\Visual Studio\External Tanius\LZ4\Tanius.LZ4.Tests\Data\test2.pcap.lz4"; // Change based on file structure of local machine
+
+            using (var fr = File.OpenRead(fileName))
+            using (var zr = new LZ4Stream(fr, LZ4StreamMode.Decompress))
+            using (var ms = new MemoryStream())
+            {
+                zr.CopyTo(ms);
+            }
+        }
+    }
+}

f0012c1a962eaf7e7161daa9597b206efbcf346b
Karl Schulze <karl@taniustech.com>
2018-01-26 15:12:43 -0800

    Test reading / writing

--- Tanius.LZ4.Tests/LZ4DataTests.cs
+++ Tanius.LZ4.Tests/LZ4DataTests.cs
@@ -33,5 +33,33 @@ namespace Tanius.LZ4.Tests
                 zr.CopyTo(ms);
             }
         }
+
+        [Test]
+        public void ReadWriteSample()
+        {
+            byte[] buffer = new byte[10 * 1024 * 1024];
+            var nRand = new Random(DateTime.Now.Millisecond);
+            nRand.NextBytes(buffer);
+            var mr = new MemoryStream(buffer);
+            var ms = new MemoryStream();
+            var zw = new LZ4Stream(ms, LZ4StreamMode.Compress);
+
+            mr.CopyTo(zw);
+            zw.Flush();
+
+            ms.Position = 0;
+            var zr = new LZ4Stream(ms, LZ4StreamMode.Decompress);
+            var mz = new MemoryStream();
+            zr.CopyTo(mz);
+
+            var clonedBuffer = mz.ToArray();
+
+            Assert.AreEqual(buffer.Length, clonedBuffer.Length);
+
+            for (int ix = 0; ix < buffer.Length; ix++)
+            {
+                Assert.AreEqual(buffer[ix], clonedBuffer[ix]);
+            }
+        }
     }
 }
